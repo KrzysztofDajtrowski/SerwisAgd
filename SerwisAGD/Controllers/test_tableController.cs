752768deafec23dbd4e2e701f4e80de1de03489f
KrzysztofDajtrowski <krzysztofdajtrowski@gmail.com>
2020-07-06 19:43:49 +0200

    zmiany

---
+++ SerwisAGD/Controllers/test_tableController.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using SerwisAGD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SerwisAGD.Controllers
+{
+    public class test_tableController : Controller
+    {
+        // GET: test_table
+        public ActionResult Index()
+        {
+            return View(DapperORM.ReturnList<test_tableModels>("ViewAll", null));
+        }
+        [HttpGet]
+        public ActionResult AddOrEdit(int id=0)
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(test_tableModels tes)
+        {
+            DynamicParameters param = new DynamicParameters();
+            param.Add("@ID", tes.ID);
+            param.Add("@Name", tes.Name);
+            param.Add("@Surname", tes.Surname);
+            DapperORM.ExecuteWithoutReturn("AddOrEdit", param);
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ SerwisAGD/Models/DapperORM.cs
@@ -0,0 +1,48 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace SerwisAGD.Models
+{
+    public static class DapperORM
+    {
+        //private static string connectionString = @"Server=sql.freeasphost.net\MSSQL2016; Database = majorkupricz_serwisAGD; User Id = majorkupricz; Password = Ax231081990; Integrated Security = True;";
+        private static string connectionString = @"Data Source=sql.freeasphost.net\MSSQL2016;" + "Initial Catalog=majorkupricz_serwisAGD;" +
+  "User id=majorkupricz;" +
+  "Password=Ax231081990;";
+
+        public static void ExecuteWithoutReturn(string ProcedureName, DynamicParameters param)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                sqlCon.Execute(ProcedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+        // DapperORM.ExecuteReturnScalar<int>(_,_);
+        public static T ExecuteReturnScalar<T>(string ProcedureName, DynamicParameters param)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+              return (T)Convert.ChangeType(sqlCon.ExecuteScalar(ProcedureName, param, commandType: CommandType.StoredProcedure), typeof (T));
+            }
+
+        }
+        //DapperORM.ReturnList<EmployeeModel> <= IEnumerable<EmployeeModel>
+        public static IEnumerable<T> ReturnList<T>(string ProcedureName, DynamicParameters param)
+        {
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                return sqlCon.Query<T>(ProcedureName, param, commandType: CommandType.StoredProcedure);
+            }
+
+        }
+    }
+}
